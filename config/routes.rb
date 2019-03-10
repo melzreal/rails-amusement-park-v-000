886da24f03d0218767249f5788aad5327b650220
Melissa Real <melissa.real@hotmail.com>
2019-03-10 10:24:25 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 resources :users, only: [:new, :create, :show]
 resources :rides, only: [:new, :create, :show]
+resources :attractions
 
    get '/signin' => 'sessions#new'
    post '/signin' => 'sessions#create'
@@ -9,9 +10,7 @@ resources :rides, only: [:new, :create, :show]
    post '/login' => 'sessions#create'
    get '/logout' => 'sessions#destroy'
    get '/attractions' => 'attractions#home'
-   resources :attractions
 
-   patch '/attractions/:id'=> 'attractions#update'
 
   root 'users#home'
 
